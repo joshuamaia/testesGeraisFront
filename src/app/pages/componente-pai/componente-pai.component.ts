ad8021fc451e74e9d896256f99aa4a1e1cb9dadd
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2024-06-25 16:24:10 -0300

    feat: implements storeService in componente-pai

--- src/app/pages/componente-pai/componente-pai.component.ts
+++ src/app/pages/componente-pai/componente-pai.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared/shared.module';
 import { ComponenteFilhoComponent } from '../componente-filho/componente-filho.component';
 import swal from 'sweetalert2';
+import { StoreService } from '../../../@core/services/store.service';
 
 @Component({
   selector: 'app-componente-pai',
@@ -10,14 +11,20 @@ import swal from 'sweetalert2';
   templateUrl: './componente-pai.component.html',
   styleUrl: './componente-pai.component.scss',
 })
-export class ComponentePaiComponent {
+export class ComponentePaiComponent implements OnInit {
   name: string = 'Nome qualquer';
 
-  funcaoPai(nameInput: string) {
-    swal.fire(`Info`, `${nameInput} - Chama Pai no filho`, 'info');
-  }
-
-  funcaoFilho(event: string) {
-    swal.fire(`Info`, event, 'info');
+  constructor(private storeService: StoreService) {}
+ 
+  ngOnInit(): void {
+    this.storeService.data$.subscribe({
+      next: (res) => {
+        const text = `${res} - o componente filho se inscreve na stream e abre modal toda vez que há uma emissão na stream`;
+        swal.fire(`Info`, text, 'info');
+      }, 
+      error: (error) => {
+        throw new Error(error)
+      }
+    })
   }
 }
